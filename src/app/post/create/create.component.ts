403b39caaed83b1b221f21beedf394ee9ee2399b
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-10-19 14:28:08 +0700

    update all component and module

--- src/app/post/create/create.component.ts
+++ src/app/post/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(
+    public postService: PostService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.postService.create(this.form.value).subscribe(res => {
+      console.log('Post created successfully!');
+      this.router.navigateByUrl('post/index');
+    })
   }
 
 }
--- src/app/post/view/view.component.ts
+++ src/app/post/view/view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  post!: Post;
+
+  constructor(
+    public postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+   ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+
+    this.postService.find(this.id).subscribe((data: Post)=>{
+      this.post = data;
+    });
   }
 
 }
